775cfffdebae2630186cb31896d2f6df22168f7c
pranaybhujade <pranaybhujade@gmail.com>
2022-10-12 23:26:29 +0530

    validate password rule 4

--- Regex study/src/com/bridgelabz/Regex.java
+++ Regex study/src/com/bridgelabz/Regex.java
@@ -4,87 +4,90 @@ package com.bridgelabz;
 import java.util.regex.Pattern;
 
 public class Regex {
-    public static void main(String[] args) {
-        //        firstName or lastName starts with Cap and has minimum 3 characters
-        Pattern pattern = Pattern.compile("^[A-Z][a-zA-Z]{2,}");
+    public void validateName(String name){
+//        firstName or lastName starts with Cap and has minimum 3 characters
+        Pattern pattern=Pattern.compile("^[A-Z][a-zA-Z]{2,}");
         boolean isValid = pattern.matcher(name).matches();
-        if (isValid) {
-            System.out.println(name + " is valid");
-        } else
-            System.out.println(name + " is not valid");
+        if (isValid){
+            System.out.println(name+" is valid");
+        }else
+            System.out.println(name+" is not valid");
     }
-
-    public void validateEmail(String email) {
+    public void validateEmail(String email){
 /*        E.g. abc.xyz@bl.co.in
         Email has 3 mandatory parts (abc, bl & co) and 2 optional (xyz & in)
         with precise @ and . positions
  */
-        Pattern pattern = Pattern.compile("^[a-zA-Z0-9]+([._-][a-zA-Z0-9]+)*@([a-z]+)([.][a-z])*(\\.[a-z]+)$");
+        Pattern pattern=Pattern.compile("^[a-zA-Z0-9]+([._-][a-zA-Z0-9]+)*@([a-z]+)([.][a-z])*(\\.[a-z]+)$");
         boolean isValid = pattern.matcher(email).matches();
-        if (isValid) {
+        if (isValid){
             System.out.println("email is valid");
-        } else
+        }else
             System.out.println("email is not valid");
     }
-
-    public void validateMobileNo(String mobileNo) {
-        Pattern pattern = Pattern.compile("^[\\d]{2}\\s[\\d]{10}$");
+    public void validateMobileNo(String mobileNo){
+        Pattern pattern=Pattern.compile("^[\\d]{2}\\s[\\d]{10}$");
         boolean isValid = pattern.matcher(mobileNo).matches();
-        if (isValid) {
+        if (isValid){
             System.out.println("mobile is valid");
-        } else {
+        }else {
             System.out.println("mobile is not valid");
         }
     }
-
-    public void validatePassword(String password) {
-        boolean isValid = true;
+    public void validatePassword(String password){
+        boolean isValid=true;
         validatePassRule1(password);
         validatePassRule2(password);
         validatePassRule3(password);
+        validatePassRule4(password);
     }
-
-    boolean validatePassRule1(String password) {
-        void validatePassRule1 (String password){
-            //minimum 8 characters
-            Pattern pattern = Pattern.compile("[\\S]{8,}");
-            Boolean isValid = pattern.matcher(password).matches();
-            if (isValid) {
-                System.out.println("Rule1 Pass...");
-            } else {
-                System.out.println("Invalid Password! \nPassword must have minimum 8 characters");
-                System.out.println("Invalid Password! =>Password must have minimum 8 characters");
-            }
-            return isValid;
-        }
-
-        boolean validatePassRule2 (String password){
-            void validatePassRule2 (String password){
-                //aAt least one upperCase letter
-                Pattern pattern = Pattern.compile("(?=.*[A-Z])[\\S]{8,}");
-                Boolean isValid = pattern.matcher(password).matches();
-                if (isValid) {
-                    System.out.println("Rule2 Pass...");
-                } else {
-                    System.out.println("Invalid Password! \npassword must have at least one uppercase letter");
-                    System.out.println("Invalid Password! => password must have at least one uppercase letter");
-                }
-            }
-            void validatePassRule3 (String password){
-                //aAt least one upperCase letter
-                Pattern pattern = Pattern.compile("(?=.*[A-Z])(?=.*[0-9])[\\S]{8,}");
-                Boolean isValid = pattern.matcher(password).matches();
-                if (isValid) {
-                    System.out.println("Rule3 Pass...");
-                } else {
-                    System.out.println("Invalid Password! => password must have at least numeric letter");
-                }
-                return isValid;
-
-
-            }
+    void validatePassRule1(String password){
+        //minimum 8 characters
+        Pattern pattern=Pattern.compile("[\\S]{8,}");
+        Boolean isValid=pattern.matcher(password).matches();
+        if (isValid){
+            System.out.println("Rule1 Pass...");
+        }else {
+            System.out.println("Invalid Password! =>Password must have minimum 8 characters");
         }
     }
-}
+
+    void validatePassRule2(String password){
+        //aAt least one upperCase letter
+        //At least one upperCase letter
+        Pattern pattern=Pattern.compile("(?=.*[A-Z])[\\S]{8,}");
+        Boolean isValid=pattern.matcher(password).matches();
+        if (isValid){
+            System.out.println("Rule2 Pass...");
+        }else {
+            System.out.println("Invalid Password! => password must have at least one uppercase letter");
+        }
+    }
+    void validatePassRule3(String password){
+        //aAt least one upperCase letter
+        Pattern pattern=Pattern.compile("(?=.*[A-Z])(?=.*[0-9])[\\S]{8,}");
+        Boolean isValid=pattern.matcher(password).matches();
+        if (isValid){
+            System.out.println("Rule3 Pass...");
+        }else {
+            System.out.println("Invalid Password! => password must have at least numeric letter");
+        }
+    }
+
+    void validatePassRule4(String password){
+        //aAt least one upperCase letter
+        Pattern pattern=Pattern.compile("(?=.*[A-Z])(?=.*[^\\w\\d\\s:])(?=.*[0-9])[\\S]{8,}");
+        Boolean isValid=pattern.matcher(password).matches();
+        if (isValid){
+            System.out.println("Rule4 Pass...");
+        }else {
+            System.out.println("Invalid Password! => password must have one special character");
+
+
+
+            }
+        }
+    }
+
 
 
